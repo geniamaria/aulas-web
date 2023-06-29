d0426a78305bd9521e83b2ae132e6eea3666b46e
geniamaria <mariageniaabilio@gmail.com>
2023-06-29 21:32:43 +0200

    adicionando a aula-21 jquery

---
+++ aula-21/aula-21.js
@@ -0,0 +1,70 @@
+
+//selecionando usando selector de tag
+$('span').text("Texto adicionado usando selector de tag");
+
+//selecionando usando selector de classe
+$('.selector-classe').text("Texto adicionado usando selector de classe");
+
+//selecionando usando selector de id
+$('#selector-id').text("Texto adicionado usando selector de id");
+
+//selecionando usando selector de descendencia
+$('.example #selector-desc').text("Texto adicionado usando selector de descendencia");
+
+//selecionando usando selector de descendencia
+$('.example > span').text("Texto adicionado usando selector de descendencia");
+
+//selecionando apenas os elementos pares dos elementos encontrados
+$('.outro-selector-par:even').text('oiiii');
+
+//selecionando apenas os elementos impares dos elementos encontrados
+$('.outro-selector-impar:odd').text('oiiii');
+
+//adicionando tags usando jQuery
+$('#adicionar-tag').append('<h1>Meu titulo jQuery</h1>');
+
+//adicionando texto
+$('#adicionar-texto p').text("Paragrafo adicionado usando a funcao 'text' do jQuery");
+
+//removendo elementos
+$('#remover-elementp').remove();
+
+//verificando estilos css de um elemento
+console.log($('#modificar-css p:first').css('font-size'));
+
+//colocando estilos css
+$('#modificar-css p:last').css({
+    'color' : '#e5e5e5',
+    'background-color' : 'black',
+    'border-radius' : '20px',
+    'width' : '80%',
+    'padding' : '5px',
+});
+
+//exemplo da animacao hide.
+$('#hide').click(function () { 
+    $('#hide').hide();
+});
+
+//exemplo da animacao show.
+$('#show').hide();
+$("#show-click").click(function () { 
+    let aleatorio = Math.floor(Math.random() * 100);
+    $('#show').show();
+    $("#show").text(aleatorio);
+});
+
+//exemplo da animacao slideDown.
+$('#slideDown').hide();
+$("#slideDown-click").click(function () { 
+    $('#slideDown').slideDown(10000);
+});
+
+//exemplo da animacao fadeIn.
+$('#fadeIn').hide();
+$("#fadeIn-click").click(function () { 
+    $('#fadeIn').fadeIn(3000);
+});
+
+
+
